8c58bf28cbcdfc0a3314a6e3433de0da6796c0bd
Pranay Ranjan <pranay2063@users.noreply.github.com>
2015-11-15 01:17:17 +0530

    Codeforces API extraction to print contests

---
+++ contest.py
@@ -0,0 +1,18 @@
+
+#Python script to show all contests' details
+#Author : Pranay Ranjan
+
+from urllib import urlopen
+from json import load
+
+response = urlopen("http://codeforces.com/api/contest.list?gym=true")
+json_obj = load(response)
+
+for obj in json_obj['result']:
+	print("\nContest ID : "+str(obj['id']))
+	print("Name : "+obj['name'])
+	print("Type : "+str(obj['type']))
+	print("Phase : "+str(obj['phase']))
+	#print("Start : "+str(obj['startTimeSeconds']))
+	print("Duration : "+str(obj['durationSeconds']))
+
