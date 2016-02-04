92d6af89fe336e619f0145f8d434870a19d8809f
Pranay Ranjan <pranay2063@users.noreply.github.com>
2016-02-04 21:17:16 +0530

    open web browser and any url

---
+++ Selenium/Open.py
@@ -0,0 +1,19 @@
+
+# open web browser and any url
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+type(browser)
+browser.get('https://gabrielecirulli.github.io/2048/')
+
+
+
+
+
+
+
+
+	
+ 
