9ff7d80e05647bfac8baa813523c64c6d22b91a0
Pranay Ranjan <pranay2063@users.noreply.github.com>
2016-02-04 21:19:42 +0530

    automate key pressing in a browser

---
+++ Selenium/2048bot.py
@@ -0,0 +1,20 @@
+
+# automate key pressing in a browser
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+browser = webdriver.Firefox()
+
+browser.get('https://gabrielecirulli.github.io/2048/')
+html = browser.find_element_by_tag_name('html')
+
+while True:
+	html.send_keys(Keys.UP)
+	html.send_keys(Keys.RIGHT)
+	html.send_keys(Keys.DOWN)
+	html.send_keys(Keys.LEFT)
+
+# browser.quit()
+
+
+
