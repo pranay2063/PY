72f7b8e9628ebe9b0d5140989137917172d66db1
Pranay Ranjan <pranay2063@users.noreply.github.com>
2023-02-06 03:29:37 +0530

    Delete NumPY directory

--- NumPY/Array.py
+++
@@ -1,6 +0,0 @@
-
-#creating an array in numpy
-import numpy as np
-import time
-a = np.array([ 1, 2, 3, 4])
-print(a)
--- NumPY/Zeros.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a = np.zeros((1,5))
-print(a)
-
-a = np.zeros((1,5), dtype=int)
-print(a)
-
-print(np.zeros((5), dtype=int))
